941a8da672769b510a91c6fc7f552c5d7fd4e443
Brinc <serapiny@gmail.com>
2023-07-25 17:46:44 +0900

    study0725

---
+++ src/org/sp/app0725/calendar/NumCell.java
@@ -0,0 +1,12 @@
+package org.sp.app0725.calendar;
+
+import java.awt.Color;
+
+//날짜를 표현할 셀
+public class NumCell extends Cell{
+	public NumCell(Color color, int width, int height) {
+		super(color, width, height);
+		
+		//this.setAlignmentX(LEFT_ALIGNMENT);
+	}
+}
